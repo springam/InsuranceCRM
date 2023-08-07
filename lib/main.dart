021fdbd1cf06832acee6501acd8bbc93a617b395
Gwangam Kim <praha3002@gmail.com>
2023-08-08 04:49:05 +0900

    안드로이드에서 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' as prefix;
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
@@ -14,10 +14,10 @@ import 'package:http/http.dart' as http;
 import 'kakao_login.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  WidgetsFlutterBinding.ensureInitialized();
   setPathUrlStrategy(); //
   KakaoSdk.init(
     nativeAppKey: '0c1d21b2ac161d3f13d08dea127666d7',
@@ -32,27 +32,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Mosaic BlueNco',
+      title: 'Mosaic Blue&co',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Mosaic Blue&co'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -61,16 +52,35 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   String accessToken = 'null';
+  int userId = 00000;
+  var userNickname = '';
+  var userEmail = 'Email 추가요청 구형해야 함';
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  Future<dynamic> getUserInfoWithKakao() async{
+    try {
+      User user = await UserApi.instance.me();
+      // userId = user.id as String;
+      // userNickname = user.kakaoAccount?.profile?.nickname as String;
+      print('사용자 정보 요청 성공'
+          '\n회원번호: ${user.id}'
+          '\n닉네임: ${user.kakaoAccount?.profile?.nickname}'
+          '\n이메일: ${user.kakaoAccount?.email}');
+
+      setState(() {
+        accessToken = 'UserID : ${user.id}/n nickname : ${user.kakaoAccount?.profile?.nickname}/n Email : ${user.kakaoAccount?.email}';
+        userId = user.id;
+        userNickname = '${user.kakaoAccount?.profile?.nickname}';
+      });
+
+    } catch (error) {
+      print('사용자 정보 요청 실패 $error');
+    }
   }
 
   Future<dynamic> kakaoTalkLogin() async{
+
+    // print(await KakaoSdk.origin);
 
     // bool isInstalled = await isKakaoTalkInstalled();
     //
@@ -78,20 +88,11 @@ class _MyHomePageState extends State<MyHomePage> {
     //     ? await UserApi.instance.loginWithKakaoTalk()
     //     : await UserApi.instance.loginWithKakaoAccount();
 
-    setState(() {
-      accessToken = '여기서 시작';
-    });
-
-    // 카카오톡 실행 가능 여부 확인
-    // 카카오톡 실행이 가능하면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
-
     if (await isKakaoTalkInstalled()) {
       try {
-        OAuthToken token =
+        // OAuthToken token =
         await UserApi.instance.loginWithKakaoTalk();
-        setState(() {
-          accessToken = token.accessToken;
-        });
+        await getUserInfoWithKakao();
       } catch (error) {
         setState(() {
           accessToken = error.toString();
@@ -103,25 +104,12 @@ class _MyHomePageState extends State<MyHomePage> {
           return;
         }
 
-        try {
-          OAuthToken token =
-          await UserApi.instance.loginWithKakaoAccount();
-          setState(() {
-            accessToken = '엑세스 토큰 : ${token.accessToken}';
-          });
-        } catch (error) {
-          setState(() {
-            accessToken = error.toString();
-          });
-        }
       }
     } else {
       try {
-        OAuthToken token =
+        // OAuthToken token =
         await UserApi.instance.loginWithKakaoAccount();
-        setState(() {
-          accessToken = '엑세스 토큰 : ${token.accessToken}';
-        });
+        await getUserInfoWithKakao();
       } catch (error) {
         setState(() {
           accessToken = error.toString();
@@ -133,42 +121,63 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Text(
-              accessToken,
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
 
-            InkWell(
-              child: Image.asset('assets/images/kakao_login_large_narrow.png'),
-              onTap: () {
-                kakaoTalkLogin();
-                // Navigator.push(context, MaterialPageRoute(builder: (context) => const Login()));
-              },
-            ),
-          ],
+    if (userId == 00000) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                accessToken,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+
+              InkWell(
+                child: Image.asset('assets/images/kakao_login_large_narrow.png'),
+                onTap: () {
+                  kakaoTalkLogin();
+                  // Navigator.push(context, MaterialPageRoute(builder: (context) => const Login()));
+                },
+              ),
+            ],
+          ),
+        ),
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text('$userNickname 님 반갑습니다.'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                accessToken,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+
+              InkWell(
+                child: Image.asset('assets/images/kakao_login_large_narrow.png'),
+                onTap: () {
+                  setState(() {
+                    userId = 00000;
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+
+
   }
 }
