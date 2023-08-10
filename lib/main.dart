37c183f42a9f9f2dd365e0f5a4f5c2ae80a7cef0
Gwangam Kim <praha3002@gmail.com>
2023-08-10 10:59:59 +0900

    웹 추가항목 동의 시 재 로그인 오류 있음

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:firebase_auth/firebase_auth.dart' as prefix;
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
@@ -37,6 +38,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      initialRoute: '',
       home: const MyHomePage(title: 'Mosaic Blue&co'),
     );
   }
@@ -52,35 +54,93 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String accessToken = 'null';
+  String accessToken = 'access token is empty';
   int userId = 00000;
   var userNickname = '';
   var userEmail = 'Email 추가요청 구형해야 함';
 
-  Future<dynamic> getUserInfoWithKakao() async{
+  Future<dynamic> getUserInfoWithKakao() async{ // 웹애서 사용자 정보 받아 오기
+
     try {
       User user = await UserApi.instance.me();
       // userId = user.id as String;
       // userNickname = user.kakaoAccount?.profile?.nickname as String;
-      print('사용자 정보 요청 성공'
+      debugPrint('사용자 정보 요청 성공'
           '\n회원번호: ${user.id}'
           '\n닉네임: ${user.kakaoAccount?.profile?.nickname}'
           '\n이메일: ${user.kakaoAccount?.email}');
 
       setState(() {
-        accessToken = 'UserID : ${user.id}/n nickname : ${user.kakaoAccount?.profile?.nickname}/n Email : ${user.kakaoAccount?.email}';
+        accessToken = 'UserID : ${user.id}\n nickname : ${user.kakaoAccount?.profile?.nickname}\n Email : ${user.kakaoAccount?.email}';
         userId = user.id;
         userNickname = '${user.kakaoAccount?.profile?.nickname}';
+        userEmail = '${user.kakaoAccount?.email}';
       });
 
     } catch (error) {
-      print('사용자 정보 요청 실패 $error');
+      debugPrint('사용자 정보 요청 실패 $error');
     }
   }
 
-  Future<dynamic> kakaoTalkLogin() async{
+  Future<dynamic> authorizeNewScope() async{ //추가 동의 받기
+    try {
+      debugPrint('추가 항목 동의 받기 진입');
+      await AuthCodeClient.instance.authorizeWithNewScopes(
+        scopes: ['friends', 'talk_message', 'account_email'],
+        // redirectUri: 'https://mosaic-bluenco.web.app',
+        redirectUri: 'http://localhost:5000'
+      );
+      debugPrint('추가 항목 동의 받기 성공');
+    } catch (error) {
+      debugPrint('추가 항목 동의 받기 실패 $error');
+    }
+  }
 
-    // print(await KakaoSdk.origin);
+  Future<dynamic> loginWithAuthorize() async{  //안드로이드 사용자 정보와 추가 동의 동시 진행
+    User user;
+    OAuthToken token;
+    try {
+      user = await UserApi.instance.me();
+      debugPrint('사용자 정보 요청 성공'
+          '\n회원번호: ${user.id}'
+          '\n닉네임: ${user.kakaoAccount?.profile?.nickname}'
+          '\n이메일: ${user.kakaoAccount?.email}');
+
+      setState(() {
+        accessToken = 'UserID : ${user.id}\n nickname : ${user.kakaoAccount?.profile?.nickname}\n Email : ${user.kakaoAccount?.email}';
+        userId = user.id;
+        userNickname = '${user.kakaoAccount?.profile?.nickname}';
+      });
+    } catch (error) {
+      debugPrint('사용자 정보 요청 실패 $error');
+      return;
+    }
+
+    // 사용자의 추가 동의가 필요한 사용자 정보 동의 항목 확인
+    List<String> scopes = ['friends', 'talk_message' ,'account_email'];
+
+    try {
+      token = await UserApi.instance.loginWithNewScopes(scopes);
+      debugPrint('현재 사용자가 동의한 동의 항목: ${token.scopes}');
+    } catch (error) {
+      debugPrint('추가 동의 요청 실패 $error');
+      return;
+    }
+
+    try {
+      User user = await UserApi.instance.me();
+      debugPrint('사용자 정보 요청 성공'
+          '\n회원번호: ${user.id}'
+          '\n닉네임: ${user.kakaoAccount?.profile?.nickname}'
+          '\n이메일: ${user.kakaoAccount?.email}');
+    } catch (error) {
+      debugPrint('사용자 정보 요청 실패 $error');
+    }
+  }
+
+  Future<dynamic> kakaoTalkLogin() async{  //카카오 로그인
+
+    // debugPrint(await KakaoSdk.origin);
 
     // bool isInstalled = await isKakaoTalkInstalled();
     //
@@ -91,8 +151,13 @@ class _MyHomePageState extends State<MyHomePage> {
     if (await isKakaoTalkInstalled()) {
       try {
         // OAuthToken token =
-        await UserApi.instance.loginWithKakaoTalk();
-        await getUserInfoWithKakao();
+        await UserApi.instance.loginWithKakaoTalk();  //인가 코드 받기
+        if (kIsWeb) {
+          await getUserInfoWithKakao();  //인가 코드로 토근 받기
+        } else {
+          await loginWithAuthorize();  //안드로이드 항목동의까지 한번에 진행
+        }
+
       } catch (error) {
         setState(() {
           accessToken = error.toString();
@@ -105,11 +170,15 @@ class _MyHomePageState extends State<MyHomePage> {
         }
 
       }
-    } else {
+    } else {  //카카오톡 미 설치시
       try {
         // OAuthToken token =
         await UserApi.instance.loginWithKakaoAccount();
-        await getUserInfoWithKakao();
+        if (kIsWeb) {
+          await getUserInfoWithKakao();
+        } else {
+          await loginWithAuthorize();
+        }
       } catch (error) {
         setState(() {
           accessToken = error.toString();
@@ -117,6 +186,73 @@ class _MyHomePageState extends State<MyHomePage> {
       }
     }
 
+  }
+
+  Future<dynamic> getFriendsList() async{  //친구 목록 가져오기
+    try {
+      Friends friends = await TalkApi.instance.friends();
+      debugPrint('카카오톡 친구 목록 가져오기 성공'
+          '\n${friends.elements?.map((friend) => friend.profileNickname).join('\n')}');
+    } catch (error) {
+      debugPrint('카카오톡 친구 목록 가져오기 실패 $error');
+    }
+  }
+
+  Future<dynamic> messageSendMe() async{
+
+    final FeedTemplate defaultFeed = FeedTemplate(
+      content: Content(
+        title: '딸기 치즈 케익',
+        description: '#케익 #딸기 #삼평동 #카페 #분위기 #소개팅',
+        imageUrl: Uri.parse(
+            'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png'),
+        link: Link(
+            webUrl: Uri.parse('https://developers.kakao.com'),
+            mobileWebUrl: Uri.parse('https://developers.kakao.com')),
+      ),
+      itemContent: ItemContent(
+        profileText: 'Kakao',
+        profileImageUrl: Uri.parse(
+            'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png'),
+        titleImageUrl: Uri.parse(
+            'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png'),
+        titleImageText: 'Cheese cake',
+        titleImageCategory: 'cake',
+        items: [
+          ItemInfo(item: 'cake1', itemOp: '1000원'),
+          ItemInfo(item: 'cake2', itemOp: '2000원'),
+          ItemInfo(item: 'cake3', itemOp: '3000원'),
+          ItemInfo(item: 'cake4', itemOp: '4000원'),
+          ItemInfo(item: 'cake5', itemOp: '5000원')
+        ],
+        sum: 'total',
+        sumOp: '15000원',
+      ),
+      social: Social(likeCount: 286, commentCount: 45, sharedCount: 845),
+      buttons: [
+        Button(
+          title: '웹으로 보기',
+          link: Link(
+            webUrl: Uri.parse('https: //developers.kakao.com'),
+            mobileWebUrl: Uri.parse('https: //developers.kakao.com'),
+          ),
+        ),
+        Button(
+          title: '앱으로보기',
+          link: Link(
+            androidExecutionParams: {'key1': 'value1', 'key2': 'value2'},
+            iosExecutionParams: {'key1': 'value1', 'key2': 'value2'},
+          ),
+        ),
+      ],
+    );
+
+    try {
+      await TalkApi.instance.sendDefaultMemo(defaultFeed);
+      debugPrint('나에게 보내기 성공');
+    } catch (error) {
+      debugPrint('나에게 보내기 실패 $error');
+    }
   }
 
   @override
@@ -132,15 +268,20 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              if (!kIsWeb) Text("android? ${Platform.isAndroid}"),
+              if (!kIsWeb) Text("ios? ${Platform.isIOS}"),
+              if (kIsWeb) const Text("This platform is web"),
               Text(
-                accessToken,
-                style: Theme.of(context).textTheme.headlineMedium,
+                accessToken
               ),
 
+              const SizedBox(height: 10,),
+
               InkWell(
-                child: Image.asset('assets/images/kakao_login_large_narrow.png'),
-                onTap: () {
-                  kakaoTalkLogin();
+                child: Image.asset('assets/images/kakao_login_large_narrow.png', scale: 2.0,),
+                onTap: () async{
+                  userId = 000;
+                  await kakaoTalkLogin();
                   // Navigator.push(context, MaterialPageRoute(builder: (context) => const Login()));
                 },
               ),
@@ -159,18 +300,26 @@ class _MyHomePageState extends State<MyHomePage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Text(
-                accessToken,
-                style: Theme.of(context).textTheme.headlineMedium,
+                accessToken
               ),
 
-              InkWell(
-                child: Image.asset('assets/images/kakao_login_large_narrow.png'),
-                onTap: () {
-                  setState(() {
-                    userId = 00000;
-                  });
-                },
-              ),
+              // InkWell(
+              //   child: Image.asset('assets/images/kakao_login_large_narrow.png', scale: 2.0,),
+              //   onTap: () {
+              //     setState(() {
+              //       userId = 00000;
+              //     });
+              //   },
+              // ),
+
+              ElevatedButton(
+                  child: const Text('나에게 메시지 보내기'),
+                  onPressed: () async{
+                    if (kIsWeb) await authorizeNewScope();  //web 환경이면 추가 항목 동의 받기
+                    // await getFriendsList();
+                    messageSendMe();
+                  },
+              )
             ],
           ),
         ),
